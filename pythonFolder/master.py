6f7d4731d0ba2f7ba1368f2c51e73a66fd319731
Gael Hugo <gael.hugo@onemore-studio.com>
2020-02-27 17:59:35 +0100

    fix ifaddr error

--- pythonFolder/master.py
+++ pythonFolder/master.py
@@ -9,9 +9,9 @@ import ifaddr
 
 adapters = ifaddr.get_adapters()
 for adapter in adapters:
-    print "IPs of network adapter " + adapter.nice_name
+    print("IPs of network adapter {0}".format(adapter.nice_name))
     for ip in adapter.ips:
-        print "   %s/%s" % (ip.ip, ip.network_prefix)
+        print("   %s/%s" % (ip.ip, ip.network_prefix))
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--id', help='firebase id (string)')
